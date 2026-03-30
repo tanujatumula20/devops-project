e9cfc12a20a05587b96b455f743a87b1c3f60f16
tanujatumula20 <tanujatumula20@gmail.com>
2026-03-30 14:59:25 +0530

    changed to static html page

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello from Jenkins + Docker Deployment! 🚀');
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, () => {
     console.log('App running on port 3000');
